80f818e0a417360d7047a554a974fc7317b6c6e6
Drakontia <mhden@drakontia.com>
2025-12-18 12:41:46 +0900

    一旦、キャラクターのデータを挿入する

--- lib/characters.ts
+++ lib/characters.ts
@@ -2,47 +2,218 @@ import { Character, JobType } from "@/types";
 
 export const CHARACTERS: Character[] = [
   {
-    id: "char_1",
+    id: "chizuru",
     name: "character.chizuru",
     rarity: "★5",
     job: JobType.PSIONIC,
-    imgUrl: "/images/characters/chizuru.webp",
+    imgUrl: "https://nightmare.aosns.com/_media/%E3%83%81%E3%82%BA%E3%83%AB.jpg",
     startingCards: ["char_card_1", "char_card_2", "char_card_3", "char_card_4"],
     hiramekiCards: ["char_hirameki_1", "char_hirameki_2", "char_hirameki_3", "char_hirameki_4"]
   },
   {
-    id: "char_2",
-    name: "character.alice",
+    id: "yuki",
+    name: "character.yuki",
+    rarity: "★5",
+    job: JobType.STRIKER,
+    imgUrl: "https://nightmare.aosns.com/_media/%E3%83%A6%E3%82%AD.jpg",
+    startingCards: ["char_card_1", "char_card_2", "char_card_3", "char_card_4"],
+    hiramekiCards: ["char_hirameki_1", "char_hirameki_2", "char_hirameki_3", "char_hirameki_4"]
+  },
+  {
+    id: "luke",
+    name: "character.luke",
+    rarity: "★5",
+    job: JobType.HUNTER,
+    imgUrl: "https://nightmare.aosns.com/_media/%E3%83%AB%E3%83%BC%E3%82%AF.jpg",
+    startingCards: ["char_card_1", "char_card_2", "char_card_3", "char_card_4"],
+    hiramekiCards: ["char_hirameki_1", "char_hirameki_2", "char_hirameki_3", "char_hirameki_4"]
+  },
+  {
+    id: "kalipe",
+    name: "character.kalipe",
     rarity: "★5",
     job: JobType.VANGUARD,
-    imgUrl: "/images/characters/alice.png",
+    imgUrl: "https://nightmare.aosns.com/_media/%E3%82%AB%E3%83%AA%E3%83%BC%E3%83%9A.jpg",
     startingCards: ["char_card_1", "char_card_2", "char_card_3", "char_card_4"],
     hiramekiCards: ["char_hirameki_1", "char_hirameki_2", "char_hirameki_3", "char_hirameki_4"]
   },
   {
-    id: "char_3",
-    name: "character.victor",
-    rarity: "★4",
+    id: "magna",
+    name: "character.magna",
+    rarity: "★5",
+    job: JobType.VANGUARD,
+    imgUrl: "https://nightmare.aosns.com/_media/%E3%83%9E%E3%82%B0%E3%83%8A.jpg",
+    startingCards: ["char_card_1", "char_card_2", "char_card_3", "char_card_4"],
+    hiramekiCards: ["char_hirameki_1", "char_hirameki_2", "char_hirameki_3", "char_hirameki_4"]
+  },
+    {
+    id: "rin",
+    name: "character.rin",
+    rarity: "★5",
+    job: JobType.STRIKER,
+    imgUrl: "https://nightmare.aosns.com/_media/%E3%83%AA%E3%83%B3.jpg",
+    startingCards: ["char_card_1", "char_card_2", "char_card_3", "char_card_4"],
+    hiramekiCards: ["char_hirameki_1", "char_hirameki_2", "char_hirameki_3", "char_hirameki_4"]
+  },
+  {
+    id: "orlea",
+    name: "character.orlea",
+    rarity: "★5",
+    job: JobType.CONTROLLER,
+    imgUrl: "https://nightmare.aosns.com/_media/%E3%82%AA%E3%83%AB%E3%83%AC%E3%82%A2.jpg",
+    startingCards: ["char_card_1", "char_card_2", "char_card_3", "char_card_4"],
+    hiramekiCards: ["char_hirameki_1", "char_hirameki_2", "char_hirameki_3", "char_hirameki_4"]
+  },
+  {
+    id: "meiling",
+    name: "character.meiling",
+    rarity: "★5",
+    job: JobType.STRIKER,
+    imgUrl: "https://nightmare.aosns.com/_media/%E3%83%A1%E3%82%A4%E3%83%AA%E3%83%B3.jpg",
+    startingCards: ["char_card_1", "char_card_2", "char_card_3", "char_card_4"],
+    hiramekiCards: ["char_hirameki_1", "char_hirameki_2", "char_hirameki_3", "char_hirameki_4"]
+  },
+  {
+    id: "belonica",
+    name: "character.belonica",
+    rarity: "★5",
     job: JobType.RANGER,
-    imgUrl: "/images/characters/victor.png",
+    imgUrl: "https://nightmare.aosns.com/_media/%E3%83%99%E3%83%AD%E3%83%8B%E3%82%AB.jpg",
     startingCards: ["char_card_1", "char_card_2", "char_card_3", "char_card_4"],
     hiramekiCards: ["char_hirameki_1", "char_hirameki_2", "char_hirameki_3", "char_hirameki_4"]
   },
   {
-    id: "char_4",
-    name: "character.emma",
-    rarity: "★4",
+    id: "renoa",
+    name: "character.renoa",
+    rarity: "★5",
     job: JobType.HUNTER,
-    imgUrl: "/images/characters/emma.png",
+    imgUrl: "https://nightmare.aosns.com/_media/%E3%83%AC%E3%83%8E%E3%82%A2.jpg",
     startingCards: ["char_card_1", "char_card_2", "char_card_3", "char_card_4"],
     hiramekiCards: ["char_hirameki_1", "char_hirameki_2", "char_hirameki_3", "char_hirameki_4"]
   },
   {
-    id: "char_5",
-    name: "character.kyle",
+    id: "hugo",
+    name: "character.hugo",
+    rarity: "★5",
+    job: JobType.RANGER,
+    imgUrl: "https://nightmare.aosns.com/_media/%E3%83%92%E3%83%A5%E3%83%BC%E3%82%B4.jpg",
+    startingCards: ["char_card_1", "char_card_2", "char_card_3", "char_card_4"],
+    hiramekiCards: ["char_hirameki_1", "char_hirameki_2", "char_hirameki_3", "char_hirameki_4"]
+  },
+  {
+    id: "hal",
+    name: "character.hal",
+    rarity: "★5",
+    job: JobType.STRIKER,
+    imgUrl: "https://nightmare.aosns.com/_media/%E3%83%8F%E3%83%AB.jpg",
+    startingCards: ["char_card_1", "char_card_2", "char_card_3", "char_card_4"],
+    hiramekiCards: ["char_hirameki_1", "char_hirameki_2", "char_hirameki_3", "char_hirameki_4"]
+  },
+  {
+    id: "kaylon",
+    name: "character.kaylon",
+    rarity: "★5",
+    job: JobType.PSIONIC,
+    imgUrl: "https://nightmare.aosns.com/_media/%E3%82%AB%E3%82%A4%E3%83%AD%E3%83%B3.jpg",
+    startingCards: ["char_card_1", "char_card_2", "char_card_3", "char_card_4"],
+    hiramekiCards: ["char_hirameki_1", "char_hirameki_2", "char_hirameki_3", "char_hirameki_4"]
+  },
+  {
+    id: "nia",
+    name: "character.nia",
     rarity: "★4",
     job: JobType.CONTROLLER,
-    imgUrl: "/images/characters/kyle.png",
+    imgUrl: "https://nightmare.aosns.com/_media/%E3%83%8B%E3%82%A2.jpg",
+    startingCards: ["char_card_1", "char_card_2", "char_card_3", "char_card_4"],
+    hiramekiCards: ["char_hirameki_1", "char_hirameki_2", "char_hirameki_3", "char_hirameki_4"]
+  },
+  {
+    id: "selena",
+    name: "character.selena",
+    rarity: "★4",
+    job: JobType.RANGER,
+    imgUrl: "https://nightmare.aosns.com/_media/%E3%82%BB%E3%83%AC%E3%83%BC%E3%83%8A.jpg",
+    startingCards: ["char_card_1", "char_card_2", "char_card_3", "char_card_4"],
+    hiramekiCards: ["char_hirameki_1", "char_hirameki_2", "char_hirameki_3", "char_hirameki_4"]
+  },
+  {
+    id: "tresa",
+    name: "character.tresa",
+    rarity: "★4",
+    job: JobType.PSIONIC,
+    imgUrl: "https://nightmare.aosns.com/_media/%E3%83%88%E3%83%AC%E3%82%B5.jpg",
+    startingCards: ["char_card_1", "char_card_2", "char_card_3", "char_card_4"],
+    hiramekiCards: ["char_hirameki_1", "char_hirameki_2", "char_hirameki_3", "char_hirameki_4"]
+  },
+  {
+    id: "amir",
+    name: "character.amir",
+    rarity: "★4",
+    job: JobType.VANGUARD,
+    imgUrl: "https://nightmare.aosns.com/_media/%E3%82%A2%E3%83%9F%E3%83%BC%E3%83%AB.jpg",
+    startingCards: ["char_card_1", "char_card_2", "char_card_3", "char_card_4"],
+    hiramekiCards: ["char_hirameki_1", "char_hirameki_2", "char_hirameki_3", "char_hirameki_4"]
+  },
+  {
+    id: "lucas",
+    name: "character.lucas",
+    rarity: "★4",
+    job: JobType.HUNTER,
+    imgUrl: "https://nightmare.aosns.com/_media/%E3%83%AB%E3%83%BC%E3%82%AB%E3%82%B9.jpg",
+    startingCards: ["char_card_1", "char_card_2", "char_card_3", "char_card_4"],
+    hiramekiCards: ["char_hirameki_1", "char_hirameki_2", "char_hirameki_3", "char_hirameki_4"]
+  },
+  {
+    id: "maribelle",
+    name: "character.maribelle",
+    rarity: "★4",
+    job: JobType.VANGUARD,
+    imgUrl: "https://nightmare.aosns.com/_media/%E3%83%9E%E3%83%AA%E3%83%99%E3%83%AB.jpg",
+    startingCards: ["char_card_1", "char_card_2", "char_card_3", "char_card_4"],
+    hiramekiCards: ["char_hirameki_1", "char_hirameki_2", "char_hirameki_3", "char_hirameki_4"]
+  },
+  {
+    id: "mika",
+    name: "character.mika",
+    rarity: "★4",
+    job: JobType.CONTROLLER,
+    imgUrl: "https://nightmare.aosns.com/_media/%E3%83%9F%E3%82%AB.jpg",
+    startingCards: ["char_card_1", "char_card_2", "char_card_3", "char_card_4"],
+    hiramekiCards: ["char_hirameki_1", "char_hirameki_2", "char_hirameki_3", "char_hirameki_4"]
+  },
+  {
+    id: "beril",
+    name: "character.beril",
+    rarity: "★4",
+    job: JobType.RANGER,
+    imgUrl: "https://nightmare.aosns.com/_media/%E3%83%99%E3%83%AA%E3%83%AB.jpg",
+    startingCards: ["char_card_1", "char_card_2", "char_card_3", "char_card_4"],
+    hiramekiCards: ["char_hirameki_1", "char_hirameki_2", "char_hirameki_3", "char_hirameki_4"]
+  },
+  {
+    id: "kacius",
+    name: "character.kacius",
+    rarity: "★4",
+    job: JobType.CONTROLLER,
+    imgUrl: "https://nightmare.aosns.com/_media/%E3%82%AB%E3%82%B7%E3%82%A6%E3%82%B9.jpg",
+    startingCards: ["char_card_1", "char_card_2", "char_card_3", "char_card_4"],
+    hiramekiCards: ["char_hirameki_1", "char_hirameki_2", "char_hirameki_3", "char_hirameki_4"]
+  },
+  {
+    id: "owen",
+    name: "character.owen",
+    rarity: "★4",
+    job: JobType.STRIKER,
+    imgUrl: "https://nightmare.aosns.com/_media/%E3%82%AA%E3%83%BC%E3%82%A6%E3%82%A7%E3%83%B3.jpg",
+    startingCards: ["char_card_1", "char_card_2", "char_card_3", "char_card_4"],
+    hiramekiCards: ["char_hirameki_1", "char_hirameki_2", "char_hirameki_3", "char_hirameki_4"]
+  },
+    {
+    id: "ray",
+    name: "character.ray",
+    rarity: "★4",
+    job: JobType.CONTROLLER,
+    imgUrl: "https://nightmare.aosns.com/_media/%E3%83%AC%E3%82%A4.jpg",
     startingCards: ["char_card_1", "char_card_2", "char_card_3", "char_card_4"],
     hiramekiCards: ["char_hirameki_1", "char_hirameki_2", "char_hirameki_3", "char_hirameki_4"]
   }
